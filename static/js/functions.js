18824f209433e583ab07cabf6a56d9a0daee98b6
Sharalyn <sharalyn.delacerna@gsfe.tupcavite.edu.ph>
2021-06-16 14:31:02 +0800

    4th

--- static/js/functions.js
+++
@@ -1,8 +0,0 @@
-
-var AddedtoCart = document.getElemetsByClassName('add-btn')
-
-for(var item = 0; item < AddedtoCart.length; item++){
-	AddedtoCart[item].addEventListener('click', function(){
-
-	})
-}
