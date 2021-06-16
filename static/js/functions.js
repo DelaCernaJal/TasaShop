3add6cd96d00c223e6878868c84f40a21b22a00b
Sharalyn <sharalyn.delacerna@gsfe.tupcavite.edu.ph>
2021-06-16 12:55:49 +0800

    second commit para sa static

---
+++ static/js/functions.js
@@ -0,0 +1,8 @@
+
+var AddedtoCart = document.getElemetsByClassName('add-btn')
+
+for(var item = 0; item < AddedtoCart.length; item++){
+	AddedtoCart[item].addEventListener('click', function(){
+
+	})
+}
